fb4380b1effb9eb42da4d154afbac7f64b0bbd4c
SPaul <stanescupaul056@gmail.com>
2026-02-18 21:45:48 +0200

    Implementare error handleing global

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(globalHandler);
+app.use(globalHandler); // aici se arunca erorile din throwController
 
 export default app;
---
+++ BackEnd/src/utils/throwController.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+export const asyncCatch = (throwController: Function) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    throwController(req, res, next).catch((err: Error) => next(err));
+  };
+};
